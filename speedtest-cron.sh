646618a9fe5df9f96ff9fcfe72f4b2d674fe329d
Adrian Campos <adriancampos@users.noreply.github.com>
2017-01-26 19:21:24 -0800

    Update speedtest-cron.sh

--- speedtest-cron.sh
+++ speedtest-cron.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Old method of logging:
-# date >> /home/pi/speedtest-log/speedtest.log
-# /usr/local/bin/speedtest --simple >> /home/pi/speedtest-log/speedtest.log
-
 # Log with Google Sheets:
 python3 /home/pi/speedtest-log/speedtest-and-upload.py
 
